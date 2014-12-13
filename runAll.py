2dc87807a639a3a6fe23f32fca20d448c27c5b83
intuinno <intuinno@gmail.com>
2014-12-13 03:16:15 +0000

    changed runAll script

--- runAll.py
+++ runAll.py
@@ -17,16 +17,19 @@ ComputeVocabulary("data/comments_study.csv","data/vocab.csv")
 ComputeCommentArticleRelevance("data/vocab.csv","data/comments_study.csv","data/articles.csv", "data/comment_study_article_relevance.csv")
 ComputeCommentConversationalRelevance("data/vocab.csv","data/comments_study.csv", "data/comment_study_comment_conversational_relevance.csv")
 
+makeCommentsListConsideringNoPicksInArticle("data/comments_study.csv", "data/articles.csv", "data/trainInputComments.csv", "data/testInputComments.csv")
 
-makeVWInputDataset(500, "data/comments_study.csv", "data/comment_study_article_relevance.csv", "data/comment_study_comment_conversational_relevance.csv", "data/trainInput.vw","data/testInput.vw")
+makeVWInputDataset( "data/trainInputComments.csv", "data/comment_study_article_relevance.csv", "data/comment_study_comment_conversational_relevance.csv", "data/trainInput.vw")
 
+makeVWInputDataset( "data/testInputComments.csv", "data/comment_study_article_relevance.csv", "data/comment_study_comment_conversational_relevance.csv", "data/testInput.vw")
 
 cmd = 'vw -d data/trainInput.vw -f  model.vw --binary '
 p = os.system(cmd)
 
-
 cmd = 'vw -t data/testInput.vw -f  model.vw -p data/prediction.vw --binary '
 p = os.system(cmd)
+
+evaluatePrediction("data/comments_study.csv","data/prediction.vw","data/result.csv")
 
 
 
